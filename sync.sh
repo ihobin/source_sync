585f445e96a4d9c284fdecae5c52c8d047e40cb3
ihobin <ihobin@users.noreply.github.com>
2020-05-28 12:46:36 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -11,7 +11,7 @@ IGNORES="^25/* ^26/* ^run/* ^build.mk/* .unison*"
 
 # init
 echo -e "Began: $(date)\nInotify: init"
-$UNISON $PRF
+#$UNISON $PRF
 echo -e "Ended: $(date)\n"
 
 # is running
@@ -35,16 +35,16 @@ $INOTIFYWAIT -mrq -e create,delete,modify,move $SRC_DIR | while read event; do
     path=$(echo $event | awk '{print $1}')
     path=${path/$SRC_DIR\//}
     file=$(echo $event | awk '{print $3}')
-    if [[ ! -a "$SRC_DIR/$path$file" ]]; then
-        size=($(stty size -F $TTY))
-        printf "%-${size[1]}s\r" "Not exists file: $SRC_DIR/$path$file"
-        continue
-    fi
     
     if is_ignore "$IGNORES" "$path$file"; then
+        newline=1
         size=($(stty size -F $TTY))
         printf "%-${size[1]}s\r" "Ignore file: $SRC_DIR/$path$file"
         continue
+    fi
+    if [[ $newline -eq 1 ]]; then
+        newline=0
+        printf "\n"
     fi
     
     echo -e "Began: $(date)\nInotify: $event"
